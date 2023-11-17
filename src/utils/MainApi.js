cf043aff5d2a2ea9f88613b5a6f9fdae8f457a65
ermesand <dafka100@gmail.com>
2023-11-17 17:01:05 +0300

    change: baseUrl

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://api.movies.daria.nomoredomainsrocks.ru";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://api.movies.daria.nomoredomainsrocks.ru";
+//export const BASE_URL = "http://localhost:3000";
 
 function getResponseData(res) {
   if (!res.ok) {
